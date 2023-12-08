ffb2cb763d607aa05d1da12f11f576dec09451d5
Matt Magoffin <git+matt@msqr.us>
2023-12-09 08:37:58 +1300

    Fix Spotless integration.

--- tic-tac-toe/src/main/java/coding101/ttt/TickTackToe.java
+++ tic-tac-toe/src/main/java/coding101/ttt/TickTackToe.java
@@ -2,7 +2,6 @@ package coding101.ttt;
 
 public class TickTackToe {
     public static void main(String[] args) {
-        System.out.println(
-        "Hello, world!");
+        System.out.println("Hello, world!");
     }
 }
